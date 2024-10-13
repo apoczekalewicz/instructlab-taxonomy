75f5a00bf1aa4eb09f775013aa1d3b9570f39954
ap <ap@laptop.ap>
2024-10-13 17:11:41 +0200

    commit

--- create.sh
+++ create.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/instructlab/taxonomy.git
 
 mkdir -p ./taxonomy/knowledge/cybersecurity/mrbighacker
-mkdir -p taxonomy/compositional_skills/STEM/math/arithmetic_reasoning
+mkdir -p ./taxonomy/compositional_skills/STEM/math/arithmetic_reasoning
 
 cp -av arithemtic_reasoning/qna.yaml taxonomy/compositional_skills/STEM/math/arithmetic_reasoning/qna.yaml
 cp -av mrbighacker/qna.yaml ./taxonomy/knowledge/cybersecurity/mrbighacker/qna.yaml
